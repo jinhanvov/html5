72513b68c6432204133122767c0ba41a1af060d1
jinhanvov <jagarm@naver.com>
2025-06-26 15:40:11 +0900

    올리기

---
+++ humortube/src/App.jsx
@@ -0,0 +1,32 @@
+function App() {
+
+const apple = 1
+
+  return (<div
+    className="center_div">
+<h1>____v.v____</h1>
+여행가고 싶은 지역
+<br/><br/>
+산토리니
+<br/>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/jPFFazCO3b0?si=eiioJ3W2AgT0sz0k" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+<br/><br/><br/>
+다음 가족여행 - 베니스
+<br/>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/bBTGN2VILlI?si=WIPQUxML4odr5j9Z" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+<br/><br/><br/>
+수진이랑 갈곳-이탈리아 포지타노
+  <br/>
+  <iframe width="560" height="315" src="https://www.youtube.com/embed/9CAysj6RvGg?si=w0IYGIGSA76fcqSB" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+  <br/><br/><br/>
+  박세찬이랑 테니스 칠곳
+  <br/>
+  <iframe width="560" height="315" src="https://www.youtube.com/embed/VEEQZQQfZME?si=_MTfhq-bfXFrT4_a" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+  </div>
+
+  
+  
+  )
+}
+
+export default App
